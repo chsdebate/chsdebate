957464686f14dcf9c4c919bf0403926b3ab5d254
guava25 <guave753@outlook.com>
2016-09-09 22:47:45 -0500

    Correction
    Correct

--- register_engine.php
+++ register_engine.php
@@ -4,13 +4,6 @@ require 'scripts/php/dbconnect.php';
 $username = mysqli_real_escape_string($dbconnect, $_POST['userreg']);
 $password = mysqli_real_escape_string($dbconnect, $_POST['passreg']);
 $password_confirmation = mysqli_real_escape_string($dbconnect, $_POST['passconreg']);
-
-$salt = mt_rand();
-echo $salt."<br>";
-$pass = hash('sha256', $password);
-$total = hash('sha256', $salt . $pass);
-echo $pass."<br>";
-echo $total;
 $user_check_query = "SELECT `username` FROM `login` WHERE `username` = '$username';";
 $user_check = mysqli_query($dbconnect,$user_check_query);
 $rowcount = mysqli_num_rows($user_check);
