b38de6ca1e13d76c5fc92126e5c03662f62f6763
guava25 <guave753@outlook.com>
2016-09-09 22:28:58 -0500

    Unique ID
    Unique ID added to prevent MySQL increment error

--- register_engine.php
+++ register_engine.php
@@ -8,13 +8,19 @@ if(empty($password) || empty($username)){
     header('Location: login.php?error=0');
 }
 else{
+    $pre = array("X","A","Z","D");
+    $pre_number = mt_rand(0,3);
+    $pre_and_number = $pre[$pre_number];
+    $ids = mt_rand(0,999);
+    $id = $pre_and_number.$ids;
+        
     $salt = mt_rand();
     echo $salt."<br>";
     $pass = hash('sha256', $password);
     $total = hash('sha256', $salt . $pass);
     echo $pass."<br>";
     echo $total;
-    mysqli_query($dbconnect, "INSERT INTO `login`(`username`, `password`, `salt`, `total_pass`, `acct_type`, `member`) VALUES ('$username','$pass','$salt','$total',1,'$today');");
+    mysqli_query($dbconnect, "INSERT INTO `login`(`id`,`username`, `password`, `salt`, `total_pass`, `acct_type`, `member`) VALUES ('$id',$username','$pass','$salt','$total',1,'$today');");
     header('Location: login.php?error=0');
 }
 ?>
