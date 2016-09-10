3bd9d80256199c8c82769dbbf8dfa79cfccd1e9a
guava25 <guave753@outlook.com>
2016-09-09 22:36:17 -0500

    Unique ID
    Unique ID added

--- register_engine.php
+++ register_engine.php
@@ -4,7 +4,7 @@ require 'scripts/php/dbconnect.php';
 $username = mysqli_real_escape_string($dbconnect, $_POST['userreg']);
 $password = mysqli_real_escape_string($dbconnect, $_POST['passreg']);
 $password_confirmation = mysqli_real_escape_string($dbconnect, $_POST['passconreg']);
-<<<<<<< HEAD
+
 $salt = mt_rand();
 echo $salt."<br>";
 $pass = hash('sha256', $password);
@@ -19,19 +19,24 @@ if($rowcount == 1){
     header('Location: login.php?error=4');
 }
 else {
-=======
 if(empty($password) || empty($username)){
     header('Location: login.php?error=0');
 }
 else{
+    $pre = array("X","A","Z","D");
+    $pre_number = mt_rand(0,3);
+    $pre_and_number = $pre[$pre_number];
+    $ids = mt_rand(0,999);
+    $id = $pre_and_number.$ids;
+        
     $salt = mt_rand();
     echo $salt."<br>";
     $pass = hash('sha256', $password);
     $total = hash('sha256', $salt . $pass);
     echo $pass."<br>";
     echo $total;
->>>>>>> refs/remotes/origin/master
     mysqli_query($dbconnect, "INSERT INTO `login`(`username`, `password`, `salt`, `total_pass`, `acct_type`, `member`) VALUES ('$username','$pass','$salt','$total',1,'$today');");
+    mysqli_query($dbconnect, "INSERT INTO `login`(`id`,`username`, `password`, `salt`, `total_pass`, `acct_type`, `member`) VALUES ('$id',$username','$pass','$salt','$total',1,'$today');");
     header('Location: login.php?error=0');
 }
 ?>
