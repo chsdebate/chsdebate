8383d34a8e281802f07abfdf01695a4f4b2ebfc4
chsdebate <centurydebates@outlook.com>
2016-09-12 19:31:07 -0500

    Update
    Security

--- attend_engine.php
+++ attend_engine.php
@@ -2,7 +2,7 @@
 session_start();
 require 'scripts/php/dbconnect.php';
 $user = $_SESSION['user'];
-$attending = $_POST['optradio'];
+$attending = mysqli_real_escape_string($dbconnect,$_POST['optradio']);
 //sleep(5);
 mysqli_query($dbconnect, "UPDATE `login` SET `t1`=[value-11],`t2`=[value-12],`t3`=[value-13],`t4`=[value-14],`t5`=[value-15],`t6`=[value-16],`t7`=[value-17] WHERE `username` = '$user''");
 foreach($_POST['list'] as $selected) {
