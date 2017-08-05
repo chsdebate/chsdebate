c4cebe4d28fa358b7a511d54b27ec5177dc068e7
guava25 <gauravb2569@hotmail.com>
2017-08-04 21:55:09 -0500

    Correction

--- meet_engine2.php
+++ meet_engine2.php
@@ -1,6 +1,6 @@
 <?php
 require 'scripts/php/dbconnect.php';
-$meet = "t" . $_GET['meet'];
+$meet = "t" . $_POST['meet'];
 $new_meet = mysqli_real_escape_string($dbconnect,$meet);
 mysqli_query($dbconnect, "update `login` set `$new_meet` = 0;");
 disconnect();
