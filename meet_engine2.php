6c5f0f8cbfd5de8dfa1365638f24f952f19d809a
guava25 <gauravb2569@hotmail.com>
2017-08-04 21:53:21 -0500

    Test Success

--- meet_engine2.php
+++ meet_engine2.php
@@ -1,10 +1,8 @@
 <?php
 require 'scripts/php/dbconnect.php';
-$meet = "t" + $_GET['meet'];
+$meet = "t" . $_GET['meet'];
 $new_meet = mysqli_real_escape_string($dbconnect,$meet);
-echo $new_meet;
-sleep(15);
 mysqli_query($dbconnect, "update `login` set `$new_meet` = 0;");
 disconnect();
-//header('Location: meets.php');
+header('Location: meets.php');
 ?>
