ba89f4dec14aa02b5c27b55c897af87b400d612a
guava25 <gauravb2569@hotmail.com>
2017-08-06 20:45:41 -0500

    Test10

--- debater.php
+++ debater.php
@@ -7,8 +7,8 @@ $user = $_SESSION['user'];
 $id = $_GET['id'];
 $new = mysqli_real_escape_string($dbconnect,$id);
 $query = mysqli_query($dbconnect, "select * from `login` where `id` = $new;");
-//$row = mysqli_fetch_array($query,MYSQLI_ASSOC);
-$row = mysqli_fetch_array($query);
+$row = mysqli_fetch_array($query,MYSQLI_ASSOC);
+//$row = mysqli_fetch_array($query);
 echo "<h1>" . $row . "</h1>";
 ?>
 <html lang="en">
