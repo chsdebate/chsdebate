3b5b633a08acccc3dc076c8d429a7e65671dd2f3
guava25 <gauravb2569@hotmail.com>
2017-08-27 21:26:40 -0500

    Fix

--- debater.php
+++ debater.php
@@ -77,6 +77,7 @@ $row = mysqli_fetch_array($query);
                       } 
                     });
                 }
+                setInterval(mess,10000);
             }
                         
         </script>
